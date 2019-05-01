2421f789a4ede7a249803ff49a099cdb4e33dc46
Rajesh <Rajesh@eleven01.io>
2019-05-01 14:38:49 +0530

    Updated WalletSeed html

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,13 +1,9 @@
 import HomePage from "../pages/home.f7.html";
-import AboutPage from "../pages/about.f7.html";
-import FormPage from "../pages/form.f7.html";
 import WalletHome from "../pages/wallethome.f7.html";
 import WalletSeed from "../pages/walletseed.f7.html";
 import VerifySeed from "../pages/verifyseed.f7.html";
 import GenerateWallet from "../pages/generatewallet.f7.html";
 
-import DynamicRoutePage from "../pages/dynamic-route.f7.html";
-import RequestAndLoad from "../pages/request-and-load.f7.html";
 import NotFoundPage from "../pages/404.f7.html";
 
 var routes = [
